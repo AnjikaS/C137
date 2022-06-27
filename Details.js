0abae83abe848cadc0584f7eb4f34443358f40a5
AnjikaS <73473105+AnjikaS@users.noreply.github.com>
2022-06-27 18:00:55 +0530

    Add files via upload

---
+++ Details.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {View, Text, FlatList, StyleSheet, Alert, SafeAreaView } from 'react-native';
+import { ListItem } from "react-native-elements";
+import axios from "axios";
+import {Card, Icon} from 'react-native-elements';
+
+export default class DetailsScreen extends Component{
+    constructor(){
+        super()
+        
+    }
+}
